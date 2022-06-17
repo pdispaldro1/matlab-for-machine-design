c808b4de7f1489fb9afb889b94359c26072a0e1c
pdispaldro1 <62079888+pdispaldro1@users.noreply.github.com>
2022-06-17 16:49:13 +0200

    Update verificabullonisenzacoppia.m

--- verificabullonisenzacoppia.m
+++ verificabullonisenzacoppia.m
@@ -1,7 +1,7 @@
 function verificasenzacoppia = verificabullonisenzacoppia(~)
 
 f = 9.807*input("Inserisci il valore in kg della forza che deve reggere il bullone: "); %valore in kg della forza agente sul bullone
-d = input ("Inserisci il valore del diametro del bullone: "); %valore del diametro del bullone
+d = input ("Inserisci il valore in mm del diametro del bullone: "); %valore del diametro del bullone
 a_res = input ("Inserisci il valore di Ares ricavato dalla normativa UNI 10011 relativo al diametro scelto: "); %valore di ares ricavato dalla UNI 10011 relativa al diametro scelto
 classe = input ("Inserisci il numero della classe del bullone: "); %valore della classe del bullone
 fkn = input ("Inserisci il valore tabellato di fkn: "); %valore tabellato di fkn
@@ -30,8 +30,8 @@ else
     disp ("Il bullone NON è verificato a resistenza coi dati inseriti")
 end
 
-sigma_adm_lamiera = input ("Inserisci il valore della sigma ammissibile della lamiera: "); %valore della sigma ammissibile dalla lamiera
-b = input ("Inserisci il valore dello spessore della lamiera: "); %valore dello spessore della lamiera
+sigma_adm_lamiera = input ("Inserisci il valore in N/mm^2 della sigma ammissibile della lamiera: "); %valore della sigma ammissibile dalla lamiera
+b = input ("Inserisci il valore in mm dello spessore della lamiera: "); %valore dello spessore della lamiera
 
 sigma_rif = f/(b*d*nb) %valore della tensione convenzionale
 
