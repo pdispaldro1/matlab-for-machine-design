ef7f90825dc35be432d31c40856e581689f0d228
pdispaldro1 <62079888+pdispaldro1@users.noreply.github.com>
2022-06-17 16:42:06 +0200

    Update dimensionamentocuscinettiasferaradiali.m

--- dimensionamentocuscinettiasferaradiali.m
+++ dimensionamentocuscinettiasferaradiali.m
@@ -4,8 +4,8 @@ disp("Questo tool permette di calcolare i valori da cui parte la scelta dei cusc
 
 l10h = input ("Inserisci il valore previsto di ore: "); %valore della durata prevista del cuscinetto in ore
 n = input ("Inserisci il numero di giri a cui ruota l'albero: "); %valore del numero di giri a cui ruota l'albero
-vxy= input ("Inserisci il valore della sommatoria delle reazioni vincolari agenti sul piano xy: "); %valore della sommatoria delle reazioni vincolari agenti sul piano xy
-vyz= input ("Inserisci il valore della sommatoria delle reazioni vincolari agenti sul piano yz: "); %valore della sommatoria delle reazioni vincolari agenti sul piano yz
+vxy= input ("Inserisci il valore in N della sommatoria delle reazioni vincolari agenti sul piano xy: "); %valore della sommatoria delle reazioni vincolari agenti sul piano xy
+vyz= input ("Inserisci il valore in N della sommatoria delle reazioni vincolari agenti sul piano yz: "); %valore della sommatoria delle reazioni vincolari agenti sul piano yz
 
 %Il cuscinetto a sfera non è sottoposto a carichi assiali
 
